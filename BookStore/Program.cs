2d9e9c05a3e4c118a53ad2be756b43d4187f39a9
Elias Jurado <elias.jurado@globant.com>
2024-01-03 14:22:20 -0500

    add getById api

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,9 +1,32 @@
+using BookStore.Models;
+using Branchy;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var options = builder.Configuration.GetSection(nameof(ApiSettings)).Get<ApiSettings>();
+
+builder.Services.AddScoped(_ => new MongoClient(options.ConnectionString));
+
+builder.Services.AddScoped(svc =>
+{
+    var client = svc.GetRequiredService<MongoClient>();
+    var database = client.GetDatabase(options.DatabaseName);
+    return database;
+});
+
+// register collections
+builder.Services.AddScoped(svc =>
+{
+    var db = svc.GetRequiredService<IMongoDatabase>();
+    return db.GetCollection<Book>(options.BooksCollectionName ?? Book.Collection);
+});
 
 var app = builder.Build();
 
@@ -16,4 +39,42 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.Path("/api/books", root =>
+{
+    root.MapGet(GetAll);
+    root.MapGet("{id}", GetOne);
+});
+
 app.Run();
+
+// Endpoints
+async Task<IResult> GetAll(IMongoCollection<Book> library)
+{
+    var filter = Builders<Book>.Filter.Empty;
+
+    var latest = await library
+        .Find(filter)
+        .SortByDescending(x => x.CreatedAt)
+        .Limit(10)
+        .ToListAsync();
+
+    var totalCount = await library.CountDocumentsAsync(filter);
+
+    return Results.Ok(new
+    {
+        results = latest ?? new(),
+        total = totalCount
+    });
+}
+
+async Task<IResult> GetOne(IMongoCollection<Book> book, string id)
+{
+    if (!ObjectId.TryParse(id, out _))
+        return Results.BadRequest("Invalid object _id was received");
+
+    var person = await book.Find(p => p.Id == id).SingleAsync();
+
+    return person is { }
+        ? Results.Ok(person)
+        : Results.NotFound();
+}
