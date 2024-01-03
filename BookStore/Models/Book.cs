be69d10b3b8403ba9f9642491b946b2293cf9b3f
Elias Jurado <elias.jurado@globant.com>
2024-01-03 11:55:08 -0500

    add books model

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public const string Collection = "Books";
+        [BsonId, BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Pages { get; set; }
+        // serialize dates into readable human format
+        [BsonRepresentation(BsonType.String)]
+        public DateTimeOffset CreatedAt { get; init; } = DateTimeOffset.UtcNow;
+    }
+}
